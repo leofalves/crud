19bad108cb401ce920c5ea19723193707c645067
Leo Fabiano Alves <leo.fabiano@gmail.com>
2024-03-26 14:43:29 -0300

    Adding STATELESS authorization in the endpoints

--- src/main/java/io/alveslf/crud/infra/security/SecurityConfiguration.java
+++ src/main/java/io/alveslf/crud/infra/security/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package io.alveslf.crud.infra.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -14,8 +15,12 @@ public class SecurityConfiguration {
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
 		return httpSecurity.csrf(csrf -> csrf.disable())
-				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-				.build();
+							.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+							.authorizeHttpRequests(authorize -> authorize
+													.requestMatchers(HttpMethod.POST, "/products").hasRole("ADMIN")
+													.anyRequest().authenticated()
+													)
+							.build();
 	}
 
 }
