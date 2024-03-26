9a8fe002d96d9d4174699cb99d87332504ac0401
Leo Fabiano Alves <leo.fabiano@gmail.com>
2024-03-26 09:55:00 -0300

    Delete Product

--- src/main/java/io/alveslf/crud/controllers/ProductController.java
+++ src/main/java/io/alveslf/crud/controllers/ProductController.java
@@ -1,9 +1,13 @@
 package io.alveslf.crud.controllers;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import io.alveslf.crud.domain.product.Product;
 import io.alveslf.crud.domain.product.ProductRepository;
 import io.alveslf.crud.domain.product.RequestProduct;
+import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 
 @RestController
@@ -36,14 +41,24 @@ public class ProductController {
 	}
 	
 	@PutMapping
+	@Transactional
 	public ResponseEntity updateProducct(@RequestBody @Valid RequestProduct data) {
-		System.out.println(data);
-		Product product = repository.getReferenceById(data.id());
-		product.setName(data.name());
-		product.setPrice(data.price());
-		repository.save(product);
-		return ResponseEntity.ok(data);
-		//return ResponseEntity.ok().build();
+		//System.out.println(data);
+		Optional<Product> optionalProduct = repository.findById(data.id());
+		if(optionalProduct.isPresent()) {
+			Product product = optionalProduct.get();
+			product.setName(data.name());
+			product.setPrice(data.price());
+			repository.save(product);
+			return ResponseEntity.ok(product);
+		}
+		return ResponseEntity.notFound().build();
 	}
-
+	
+	@DeleteMapping("/{id}")
+	@Transactional
+	public ResponseEntity deleteProduct(@PathVariable String id) {
+		repository.deleteById(id);
+		return ResponseEntity.ok().build();
+	}
 }
